0e5e62cf4765a1edd17961655cd6d59bafeb916f
fedotova <fedotovayo@gmail.com>
2020-10-10 14:44:02 +0300

    Scripted auto-commit on change (2020-10-10 14:44:02) by gitwatch.sh

--- src/FlightWritable.java
+++ src/FlightWritable.java
@@ -7,7 +7,10 @@ import java.io.IOException;
 
 public class FlightWritable implements Writable{
     private float arr_delay;
-    private 
+    private int dest_airport_id;
+    private float air_time;
+    private float cancelled;
+    
 
     public void readFields (DataInput in) throws IOException {
 
