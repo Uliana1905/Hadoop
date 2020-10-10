135b6d0693011846c1d9532b8c239b48af2dd36d
fedotova <fedotovayo@gmail.com>
2020-10-10 14:34:58 +0300

    Scripted auto-commit on change (2020-10-10 14:34:58) by gitwatch.sh

--- src/FlightWritable.java
+++ src/FlightWritable.java
@@ -1,3 +1,10 @@
-public class FlightWritable {
-    
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlightWritable implements Writable{
+
 }
