53a578d085cc32788dcec382e04a724d2402ade6
fedotova <fedotovayo@gmail.com>
2020-10-10 13:59:41 +0300

    Scripted auto-commit on change (2020-10-10 13:59:41) by gitwatch.sh

--- src/FlightWritableComparable.java
+++ src/FlightWritableComparable.java
@@ -1,3 +1,9 @@
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class FlightWritableComparable {
     
 }
