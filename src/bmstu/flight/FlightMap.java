e97e3b341bf1b284107e8ffcd041941a71520e11
fedotova <fedotovayo@gmail.com>
2020-10-13 13:53:22 +0300

    Scripted auto-commit on change (2020-10-13 13:53:22) by gitwatch.sh

--- src/bmstu/flight/FlightMap.java
+++ src/bmstu/flight/FlightMap.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 
@@ -12,7 +11,7 @@ import java.io.IOException;
 
 public class FlightMap  extends Mapper <FlightWritable,Text, FlightWritableComparable, Text>{
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    protected void map( key, Text value, Context context) throws IOException, InterruptedException {
         String [] str = value.toString().split(",");
 
     }
