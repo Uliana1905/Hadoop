6cbc4022928a943a701d067f832d39e9a0e3234b
fedotova <fedotovayo@gmail.com>
2020-10-13 13:36:37 +0300

    Scripted auto-commit on change (2020-10-13 13:36:37) by gitwatch.sh

--- src/bmstu/flight/FlightMap.java
+++ src/bmstu/flight/FlightMap.java
@@ -1,5 +1,14 @@
 package bmstu.flight;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+
 public class FlightMap {
-    
+
 }
