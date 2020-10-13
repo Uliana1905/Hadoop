cb76b9fd56ff522f5873deea38632eabcf987fee
fedotova <fedotovayo@gmail.com>
2020-10-13 14:34:58 +0300

    Scripted auto-commit on change (2020-10-13 14:34:58) by gitwatch.sh

--- src/bmstu/flight/FlightMap.java
+++ src/bmstu/flight/FlightMap.java
@@ -14,10 +14,15 @@ public class FlightMap  extends Mapper <LongWritable, Text, IntWritable, Text>{
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String [] str = value.toString().split(",");
 
-        if (str[21].equals ("")){
-            if ( Float.parseFloat(str[20])!= (float)0){
-                context.write  (new Text (str[20]), new IntWritable (Int))
+        if (str.length > 2) {
+            if (str[21].equals("")) {
+                if (Float.parseFloat(str[20]) != (float) 0) {
+                    context.write(new Text(str[20]), new IntWritable(Integer.parseInt(str[14])));
+                }
             }
+
+        }else{
+            
         }
 
     }
