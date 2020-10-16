c726ea1f8634de6f9b22b78e0c453fcf2f282da5
fedotova <fedotovayo@gmail.com>
2020-10-16 22:59:03 +0300

    Scripted auto-commit on change (2020-10-16 22:59:03) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightWritableComparable.java
+++ src/main/java/bmstu/flight/FlightWritableComparable.java
@@ -1,5 +1,31 @@
 package bmstu.flight;
 
-public class FlightWritableComparable {
-    
+public class FlightWritableComparable implements WritableComparable {
+    // Some data
+    private int counter;
+    private long timestamp;
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(counter);
+        out.writeLong(timestamp);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        counter = in.readInt();
+        timestamp = in.readLong();
+    }
+
+    public int compareTo(MyWritableComparable o) {
+        int thisValue = this.value;
+        int thatValue = o.value;
+        return (thisValue < thatValue ? -1 : (thisValue==thatValue ? 0 : 1));
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + counter;
+        result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+        return result
+    }
 }
