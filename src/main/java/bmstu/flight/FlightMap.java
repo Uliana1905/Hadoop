0b6db0d3b91dc38191e4d383899f67f375f150fd
fedotova <fedotovayo@gmail.com>
2020-10-16 23:19:31 +0300

    Scripted auto-commit on change (2020-10-16 23:19:31) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightMap.java
+++ src/main/java/bmstu/flight/FlightMap.java
@@ -12,36 +12,21 @@ import java.io.IOException;
 public class FlightMap  extends Mapper <LongWritable, Text, FlightWritableComparable, Text>{
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String [] str = value.toString().split(",");
+        String[] str = value.toString().split(",");
 
-        if (Float.parseFloat(str[19]) != (float) 0){
-            if (Float.parseFloat(str[19]) != (float) 0){
+        if (Float.parseFloat(str[19]) != (float) 0) {
+            if (Float.parseFloat(str[19]) != (float) 0) {
                 FlightWritableComparable flight = new FlightWritableComparable();
 
                 flight.set_flag(1);
                 flight.set_des_air(Integer.parseInt(str[14]));
 
-                Text time = 
+                Text time = new Text(str[18]);
 
-                context.write (flight, )
+                context.write(flight, time);
             }
         }
 
-
-
-
-
-
-            if (str.length > 2) {
-            if (str[21].equals("")) {
-                if (Float.parseFloat(str[20]) != (float) 0) {
-                    context.write(new Text(str[20]), new IntWritable(Integer.parseInt(str[14])));
-                }
-            }
-
-        }else{
-            context.write(new Text(str[1]), new IntWritable(Integer.parseInt(str[0])));
-        }
 
     }
 
