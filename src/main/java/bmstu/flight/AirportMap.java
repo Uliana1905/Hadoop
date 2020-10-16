51024551c58eab4706423ea9fe4091b447434275
fedotova <fedotovayo@gmail.com>
2020-10-17 00:11:57 +0300

    Scripted auto-commit on change (2020-10-17 00:11:57) by gitwatch.sh

--- src/main/java/bmstu/flight/AirportMap.java
+++ src/main/java/bmstu/flight/AirportMap.java
@@ -14,18 +14,7 @@ public class AirportMap  extends Mapper <LongWritable, Text, FlightWritableCompa
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] str = value.toString().split(",");
 
-        if (Float.parseFloat(str[19])!= (float)1 && !str[0].equals("YEAR")){
-            if (Float.parseFloat(str[18]) != (float) 0) {
-                FlightWritableComparable flight = new FlightWritableComparable();
-
-                flight.set_flag(1);
-                flight.set_des_air(Integer.parseInt(str[14]));
-
-                Text time = new Text(str[18]);
-
-                context.write(flight, time);
-            }
-        }
+        
 
 
     }
