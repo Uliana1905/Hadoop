26cca0e587943753ef8e26a52fd4a53b1a9c6992
fedotova <fedotovayo@gmail.com>
2020-10-16 23:41:46 +0300

    Scripted auto-commit on change (2020-10-16 23:41:46) by gitwatch.sh

--- src/main/java/bmstu/flight/AirportMap.java
+++ src/main/java/bmstu/flight/AirportMap.java
@@ -1,4 +1,34 @@
 package bmstu.flight;
 
-public class AirportMap {
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+
+public class AirportMap  extends Mapper <LongWritable, Text, FlightWritableComparable, Text>{
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] str = value.toString().split(",");
+
+        if (Float.parseFloat(str[19])!= (float)1 && !str[0].equals("YEAR")){
+            if (Float.parseFloat(str[18]) != (float) 0) {
+                FlightWritableComparable flight = new FlightWritableComparable();
+
+                flight.set_flag(1);
+                flight.set_des_air(Integer.parseInt(str[14]));
+
+                Text time = new Text(str[18]);
+
+                context.write(flight, time);
+            }
+        }
+
+
+    }
+
+
 }
