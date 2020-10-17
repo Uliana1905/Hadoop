5c934872e582a649a1a9ae1088c0c4bfedd89bdd
fedotova <fedotovayo@gmail.com>
2020-10-17 13:31:43 +0300

    Scripted auto-commit on change (2020-10-17 13:31:43) by gitwatch.sh

--- src/main/java/bmstu/flight/GroupingComparator.java
+++ src/main/java/bmstu/flight/GroupingComparator.java
@@ -8,3 +8,18 @@ public class GroupingComparator implements RawComparator {
         return a.compareTo(b);
     }
 }
+
+
+public class NaturalKeyGroupingComparator extends WritableComparator {
+    protected NaturalKeyGroupingComparator() {
+        super(StockKey.class, true);
+    }
+    @SuppressWarnings("rawtypes")
+    @Override
+    public int compare(WritableComparable w1, WritableComparable w2) {
+        StockKey k1 = (StockKey)w1;
+        StockKey k2 = (StockKey)w2;
+
+        return k1.getSymbol().compareTo(k2.getSymbol());
+    }
+}
