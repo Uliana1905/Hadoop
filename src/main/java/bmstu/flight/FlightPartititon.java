1de85eb64cb23c43960bf80b71d3573fd42104dc
fedotova <fedotovayo@gmail.com>
2020-10-17 12:48:04 +0300

    Scripted auto-commit on change (2020-10-17 12:48:04) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightPartititon.java
+++ src/main/java/bmstu/flight/FlightPartititon.java
@@ -2,7 +2,7 @@ import bmstu.flight.FlightWritableComparable;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.Text
 
-public class FlightPartitioner  extends Partitioner<FlightWritableComparable, Text> {
+public class FlightPartition  extends Partitioner<FlightWritableComparable, Text> {
     public int getPartition(FlightWritableComparable key, Text value, int numReduceTasks) {
         return key.getDes_air() % numReduceTasks;
     }
