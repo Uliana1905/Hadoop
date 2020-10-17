c2bdddaf99de7238e3843b6dcdad7336045d3717
fedotova <fedotovayo@gmail.com>
2020-10-17 19:19:06 +0300

    Scripted auto-commit on change (2020-10-17 19:19:06) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightJob.java
+++ src/main/java/bmstu/flight/FlightJob.java
@@ -1,19 +1,24 @@
 package bmstu.flight;
 
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.jobcontrol.Job;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
+
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 
 public class FlightJob {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException, Exception, ClassNotFoundException {
 
         Job job = Job.getInstance();
-        job.setJarByClass(JoinJob.class);
+        job.setJarByClass(FlightJob.class);
         job.setJobName("JoinJob sort");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMap.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMap.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(FlightPartition.class);
         job.setGroupingComparatorClass(GroupingComparator.class);
