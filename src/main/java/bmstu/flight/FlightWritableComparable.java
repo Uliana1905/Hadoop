c0e2c96d50eb43e1275ed18e8410663e3ee11d3b
fedotova <fedotovayo@gmail.com>
2020-10-17 12:39:12 +0300

    Scripted auto-commit on change (2020-10-17 12:39:12) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightWritableComparable.java
+++ src/main/java/bmstu/flight/FlightWritableComparable.java
@@ -9,16 +9,16 @@ import java.io.IOException;
 
 public class FlightWritableComparable implements WritableComparable {
     private int flag;
-    private int dest_air_ip;
+    private int dest_air_id;
 
     public void write(DataOutput out) throws IOException {
         out.writeInt(flag);
-        out.writeLong(dest_air_ip);
+        out.writeLong(dest_air_id);
     }
 
     public void readFields(DataInput in) throws IOException {
         flag = in.readInt();
-        dest_air_ip = in.readInt();
+        dest_air_id = in.readInt();
     }
 
     public void set_flag ( int flag){
@@ -26,20 +26,22 @@ public class FlightWritableComparable implements WritableComparable {
     }
 
     public void  set_des_air ( int dest_air_ip){
-        this.dest_air_ip = dest_air_ip;
+        this.dest_air_id = dest_air_ip;
     }
+
+    public void
 
 
     public int compareTo(FlightWritableComparable o) {
         FlightWritableComparable that = (FlightWritableComparable) o;
 
         int that_flag = that.flag;
-        int that_dest_air_ip = that.dest_air_ip;
+        int that_dest_air_ip = that.dest_air_id;
 
-        if (this.dest_air_ip > that_dest_air_ip){
+        if (this.dest_air_id > that_dest_air_ip){
             return 1;
         }
-        if (this.dest_air_ip < that_dest_air_ip){
+        if (this.dest_air_id < that_dest_air_ip){
             return -1;
         }
 
