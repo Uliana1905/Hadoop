a00c778166bee0469e65ea0b937bcd276062962e
fedotova <fedotovayo@gmail.com>
2020-10-17 13:54:58 +0300

    Scripted auto-commit on change (2020-10-17 13:54:58) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightReduce.java
+++ src/main/java/bmstu/flight/FlightReduce.java
@@ -11,8 +11,17 @@ import java.util.Iterator;
 public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+        
+
+
+
     }
+
+
+
+
+
+
 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
