4259aaaf6b4693b840cf64cc4a379c521daa5bef
fedotova <fedotovayo@gmail.com>
2020-10-19 16:37:41 +0300

    Scripted auto-commit on change (2020-10-19 16:37:41) by gitwatch.sh

--- src/main/java/bmstu/flight/GroupingComparator.java
+++ src/main/java/bmstu/flight/GroupingComparator.java
@@ -13,14 +13,14 @@ public class GroupingComparator extends WritableComparator {
     public int compare(WritableComparable a, WritableComparable b) {
         FlightWritableComparable a1 = (FlightWritableComparable) a;
         FlightWritableComparable b1 = (FlightWritableComparable) b;
-        System.out.println(a1.getDes_air());
+        System.out.println(a.toString());
 
 
         if (a1.getDes_air() > b1.getDes_air()){
             return 1;
         }
         if (a1.getDes_air() < b1.getDes_air()){
-            return -1;
+            return -1;  
         }
 
 
