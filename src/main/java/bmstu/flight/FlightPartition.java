36c3666a2b35216a5dbc2ce4d7c32dd6b132781c
fedotova <fedotovayo@gmail.com>
2020-10-19 16:14:22 +0300

    Scripted auto-commit on change (2020-10-19 16:14:22) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightPartition.java
+++ src/main/java/bmstu/flight/FlightPartition.java
@@ -5,10 +5,8 @@ import org.apache.hadoop.io.Text;
 
 public class FlightPartition  extends Partitioner<FlightWritableComparable, Text> {
     public int getPartition(FlightWritableComparable key, Text value, int numReduceTasks) {
-        if (key.getDes_air() == 0) {
-            System.out.println("            }}}}}}}}}}}}}" + key.getDes_air());
+        System.out.println(key.getDes_air());
 
-        }
         return key.getDes_air() % numReduceTasks;
     }
 }
