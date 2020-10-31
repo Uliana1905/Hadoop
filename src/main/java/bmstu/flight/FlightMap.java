fafd944125a528b391196e02e3632e84df98fb11
fedotova <fedotovayo@gmail.com>
2020-10-31 15:19:33 +0300

    Scripted auto-commit on change (2020-10-31 15:19:33) by gitwatch.sh

--- src/main/java/bmstu/flight/FlightMap.java
+++ src/main/java/bmstu/flight/FlightMap.java
@@ -12,11 +12,15 @@ import java.io.IOException;
 public class FlightMap  extends Mapper <LongWritable, Text, FlightWritableComparable, Text>{
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] str = value.toString().split(",");
+        final String REQEX_SPLITTER = ",";
+        final String COMPARE = "\"YEAR\"";
+        final int FLAG_FLIGHT = 1;
+        final float NOT_CANCELED = 0;
+        String[] str = value.toString().split(REQEX_SPLITTER);
 
-        if (!str[0].equals("\"YEAR\"") && !str[18].isEmpty()){
-            if (Float.parseFloat(str[18]) != (float) 0) {
-                FlightWritableComparable flight = new FlightWritableComparable(1,Integer.parseInt(str[14]));
+        if (!str[0].equals(COMPARE) && !str[18].isEmpty()){
+            if (Float.parseFloat(str[18]) != NOT_CANCELED) {
+                FlightWritableComparable flight = new FlightWritableComparable(FLAG_FLIGHT,Integer.parseInt(str[14]));
 
 
                 Text time = new Text(str[18]);
