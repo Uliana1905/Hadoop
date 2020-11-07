094fa0bab0ea33efa17d4cd0b2d92646b39cf5df
fedotova <fedotovayo@gmail.com>
2020-11-07 19:30:10 +0300

    Scripted auto-commit on change (2020-11-07 19:30:10) by gitwatch.sh

--- lab3/src/main/java/FlightSerializable.java
+++ lab3/src/main/java/FlightSerializable.java
@@ -2,5 +2,5 @@ import java.io.Serializable;
 
 public class FlightSerializable extends Serializable<>{
     @Override
-   public void FlightSerializable(int orig_air_id, int )
+   public void FlightSerializable(int orig_air_id, int dest_air_id, )
 }
