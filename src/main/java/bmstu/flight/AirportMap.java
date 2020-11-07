fa7cf38bf58a71971b7ba2a71ace8ddf7e2aa628
fedotova <fedotovayo@gmail.com>
2020-11-07 12:46:19 +0300

    Scripted auto-commit on change (2020-11-07 12:46:19) by gitwatch.sh

--- src/main/java/bmstu/flight/AirportMap.java
+++ src/main/java/bmstu/flight/AirportMap.java
@@ -10,16 +10,16 @@ import java.io.IOException;
 
 
 public class AirportMap  extends Mapper <LongWritable, Text, FlightWritableComparable, Text>{
+    private static final String REQEX_SPLITTER = ",(?! )";
+    private static final String REQEX_REPLACE = "\"";
+    private static final String REQEX_REPLACEMENT = "";
+    private static final String FIRST_LINE = "Code";
+    private static final int FLAG_AIRPORT = 0;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        final String REQEX_SPLITTER = ",(?! )";
-        final String REQEX_REPLACE = "\"";
-        final String REQEX_REPLACEMENT = "";
-        final String COMPARE = "Code";
-        final int FLAG_AIRPORT = 0;
         String[] str = value.toString().replaceAll(REQEX_REPLACE,REQEX_REPLACEMENT).split(REQEX_SPLITTER);
 
-        if (!str[0].equals(COMPARE)) {
+        if (!str[0].equals(FIRST_LINE)) {
             FlightWritableComparable flight = new FlightWritableComparable(FLAG_AIRPORT,Integer.parseInt(str[0]));
 
             //System.out.println(str);
--- src/main/java/bmstu/flight/FlightMap.java
+++ src/main/java/bmstu/flight/FlightMap.java
@@ -10,15 +10,17 @@ import java.io.IOException;
 
 
 public class FlightMap  extends Mapper <LongWritable, Text, FlightWritableComparable, Text>{
+
+    private static final String REQEX_SPLITTER = ",";
+    private static final String FIRST_LINE = "\"YEAR\"";
+    private static final int FLAG_FLIGHT = 1;
+    private static final float NOT_CANCELED = 0;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        final String REQEX_SPLITTER = ",";
-        final String COMPARE = "\"YEAR\"";
-        final int FLAG_FLIGHT = 1;
-        final float NOT_CANCELED = 0;
+
         String[] str = value.toString().split(REQEX_SPLITTER);
 
-        if (!str[0].equals(COMPARE) && !str[18].isEmpty()){
+        if (!str[0].equals(FIRST_LINE) && !str[18].isEmpty()){
             if (Float.parseFloat(str[18]) != NOT_CANCELED) {
                 FlightWritableComparable flight = new FlightWritableComparable(FLAG_FLIGHT,Integer.parseInt(str[14]));
 
