7e14f53a19b000fdf6ef369e6d353d770be140a9
fedotova <fedotovayo@gmail.com>
2020-11-07 13:15:40 +0300

    Scripted auto-commit on change (2020-11-07 13:15:40) by gitwatch.sh

--- lab3/src/main/java/AirportFromTo.java
+++ lab3/src/main/java/AirportFromTo.java
@@ -3,6 +3,7 @@ import org.apache.spark.SparkConf;
 public class AirportFromTo {
     public static void main ( String [] args){
         SparkConf conf = new SparkConf().setAppName("lab5");
+        JavaSparkContext sc = new JavaSparkContext ( conf);
 
     }
 }
