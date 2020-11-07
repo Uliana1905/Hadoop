c478a7855e0f8f08ceadae16cc90a57f2f5baca9
fedotova <fedotovayo@gmail.com>
2020-11-07 15:54:05 +0300

    Scripted auto-commit on change (2020-11-07 15:54:05) by gitwatch.sh

--- lab3/src/main/java/AirportFromTo.java
+++ lab3/src/main/java/AirportFromTo.java
@@ -3,14 +3,18 @@ import org.apache.spark.SparkConf;
 import java.util.Arrays;
 
 public class AirportFromTo {
+    private static final String FIRSTLINE = "Code,Description";
+    private static final String REPLACEABLE = "\"";
+    private static final String REPLACEMENT = "";
+
     public static void main ( String [] args){
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext ( conf);
 
         JavaRDD<String> fileWithAirports = sc.textFile("IDandName.csv");
 
-        JavaRDD<String> airports = fileWithAirports.Map( s-> s.replace("\"","")).filter(s-> !s.equals("Code,Description"));
-        
+        JavaRDD<String> airports = fileWithAirports.Map( s-> s.replace(REPLACEABLE,REPLACEMENT)).filter(s-> !s.equals(FIRSTLINE));
+
 
 
 
