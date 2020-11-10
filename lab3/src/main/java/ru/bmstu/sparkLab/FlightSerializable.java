7a86bc8aff5b7673123fb5b54445306a190d5497
fedotova <fedotovayo@gmail.com>
2020-11-10 17:44:29 +0300

    Scripted auto-commit on change (2020-11-10 17:44:29) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/sparkLab/FlightSerializable.java
+++ lab3/src/main/java/ru/bmstu/sparkLab/FlightSerializable.java
@@ -1,11 +1,12 @@
+package ru.bmstu.sparkLab;
+
 import java.io.Serializable;
 
-public class FlightSerializable extends Serializable<>{
+public class FlightSerializable extends Serializable<> {
     private int orig_air_id;
     private int dest_air_id;
     private int arr_delay_new;
     private int concelled;
-    @Override
    public void FlightSerializable(int orig_air_id, int dest_air_id, int arr_delay_new, int concelled){
 
         this.orig_air_id = orig_air_id;
