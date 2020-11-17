602ed839070667d35e354537dcb38893a822965c
fedotova <fedotovayo@gmail.com>
2020-11-17 16:32:48 +0300

    Scripted auto-commit on change (2020-11-17 16:32:48) by gitwatch.sh

--- lab3/src/main/java/ru/bmstu/sparkLab/FlightSerializable.java
+++ lab3/src/main/java/ru/bmstu/sparkLab/FlightSerializable.java
@@ -56,8 +56,8 @@ public class FlightSerializable implements Serializable{
 
     public static String combine (Tuple2<Integer,Integer> key, FlightSerializable value, Map<Integer,String> airportsBroadcasted ){
         String result;
-        //result = airportsBroadcasted.get(key._1) +"->"+ airportsBroadcasted.get(key._2) +"\n"+"Максимальное время"+  value.getMaxArr_delay() + " Процент опоздавших: " + (value.getNum_dellay() / value.getNum_flight()) * 100 + "%" + " Процент отмененных: " + (value.getNumCancelled() / value.getNum_flight()) * 100 + "%";
-        result = airportsBroadcasted.get(key._1) +"->"+ airportsBroadcasted.get(key._2) +"\n"+"Максимальное время"+  value.getMaxArr_delay() + " Количество полетов : " +  value.getNum_flight() + " Количество отменных: " + value.getNumCancelled() + "%";
+        result = airportsBroadcasted.get(key._1) +"->"+ airportsBroadcasted.get(key._2) +"\n"+"Максимальное время"+  value.getMaxArr_delay() + " Процент опоздавших: " + value.getNum_dellay() / value.getNum_flight() + "%" + " Процент отмененных: " + value.getNumCancelled() / value.getNum_flight() + "%";
+        //result = airportsBroadcasted.get(key._1) +"->"+ airportsBroadcasted.get(key._2) +"\n"+"Максимальное время"+  value.getMaxArr_delay() + " Количество полетов : " +  value.getNum_flight() + " Количество отменных: " + value.getNumCancelled() + "%";
 
         return result;
    }
