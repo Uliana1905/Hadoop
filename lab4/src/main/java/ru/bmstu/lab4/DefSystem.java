7e94fe1ccc9a5a1acf38cc7b84c464549d500753
fedotova <fedotovayo@gmail.com>
2020-12-09 19:20:21 +0300

    Scripted auto-commit on change (2020-12-09 19:20:21) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
+++ lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
@@ -1,4 +1,10 @@
 package ru.bmstu.lab4;
 
+import java.io.IOException;
+
+
 public class DefSystem {
+    public static void main( String []args) throws IOException {
+        ActorSystem system = ActorSystem.create("lab4");
+    }
 }
