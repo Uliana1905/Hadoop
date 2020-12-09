36288bbb01f05ad87d418fe5c1b243176295ab45
fedotova <fedotovayo@gmail.com>
2020-12-09 19:24:37 +0300

    Scripted auto-commit on change (2020-12-09 19:24:37) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
+++ lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
@@ -1,10 +1,14 @@
 package ru.bmstu.lab4;
 
+import akka.actor.ActorSystem;
+
 import java.io.IOException;
 
 
 public class DefSystem {
     public static void main( String []args) throws IOException {
         ActorSystem system = ActorSystem.create("lab4");
+
+        
     }
 }
