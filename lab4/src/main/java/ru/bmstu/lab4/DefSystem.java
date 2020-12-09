d820f73b6952b624d978f93242e80ed7dab40c62
fedotova <fedotovayo@gmail.com>
2020-12-09 19:55:20 +0300

    Scripted auto-commit on change (2020-12-09 19:55:20) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
+++ lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
@@ -1,18 +1,7 @@
 package ru.bmstu.lab4;
 
-import akka.NotUsed;
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
-import akka.http.javadsl.ConnectHttp;
 
 
-import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
@@ -28,7 +17,7 @@ public class DefSystem {
         MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+        final CompletionStage<Tcp.ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
