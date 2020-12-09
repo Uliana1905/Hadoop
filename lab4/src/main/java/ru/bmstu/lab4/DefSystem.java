26b944db1d013720ddfc5de59f92dcdd2d1e654c
fedotova <fedotovayo@gmail.com>
2020-12-09 19:50:28 +0300

    Scripted auto-commit on change (2020-12-09 19:50:28) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
+++ lab4/src/main/java/ru/bmstu/lab4/DefSystem.java
@@ -1,10 +1,19 @@
 package ru.bmstu.lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 
 public class DefSystem {
@@ -12,7 +21,21 @@ public class DefSystem {
         ActorSystem system = ActorSystem.create("lab4");
         ActorRef store_actor = system.actorOf(Props.create(StoreActor.class), "storeActor")
         ActorRef execute_actor = system.actorOf(Props.create(ExecuteActor.class), "executeActor"
-        
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 
 
