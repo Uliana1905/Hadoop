5f77126ac1493dd3db5e2dbb828b59d4f003e88e
fedotova <fedotovayo@gmail.com>
2020-12-10 17:15:31 +0300

    Scripted auto-commit on change (2020-12-10 17:15:31) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/StoreActor.java
+++ lab4/src/main/java/ru/bmstu/lab4/StoreActor.java
@@ -6,11 +6,12 @@ import akka.actor.AbstractActor;
         import java.util.HashMap;
         import java.util.Map;
 public class StoreActor extends AbstractActor {
-    private Map<String, String> store = new HashMap<>();
+    private Map<String, Map<String, String>> store = new HashMap<>();
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
+                    if (!store.containsKey(m))
                     store.put(m.getKey(), m.getValue());
                     System.out.println("receive message! "+m.toString());
                 })
