0b08ac0345f6447e8eb038b2bf0ec0dc997baf07
fedotova <fedotovayo@gmail.com>
2020-12-13 21:05:44 +0300

    Scripted auto-commit on change (2020-12-13 21:05:44) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/StoreType.java
+++ lab4/src/main/java/ru/bmstu/lab4/StoreType.java
@@ -1,6 +1,23 @@
 package ru.bmstu.lab4;
 
+import java.util.Map;
+
 public class StoreType {
-    private 
+    private Map<String, String> value;
+    private String key;
+
+    public String getKey(){
+        return key;
+    }
+
+    public Map <String, String> getValue(){
+        return value;
+    }
+
+    public StoreType (String key, Map<String, String> value ){
+        this.key = key;
+        this.value = value;
+    }
+
 
 }
