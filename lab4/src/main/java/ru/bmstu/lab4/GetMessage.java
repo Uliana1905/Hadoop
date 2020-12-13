0d8e382e27213bcc0c2cd5190a9ddcaaaed550d0
fedotova <fedotovayo@gmail.com>
2020-12-13 20:51:29 +0300

    Scripted auto-commit on change (2020-12-13 20:51:29) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/GetMessage.java
+++ lab4/src/main/java/ru/bmstu/lab4/GetMessage.java
@@ -1,5 +1,19 @@
 package ru.bmstu.lab4;
 
 public class GetMessage {
+    private String packageId;
+    private String testName;
+    private String result;
+
+    public GetMessage (String packageId, String testName, String result){
+        this.packageId = packageId;
+        this.result = result;
+        this.testName = testName;
+    }
+
+    public String getPackageId(){
+        return packageId;
+    }
+
 
 }
