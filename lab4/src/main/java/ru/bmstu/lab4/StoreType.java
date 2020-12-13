b60c9e18ddc951bee9fc8f8a96ade755e7846781
fedotova <fedotovayo@gmail.com>
2020-12-13 20:47:10 +0300

    Scripted auto-commit on change (2020-12-13 20:47:10) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/StoreType.java
+++ lab4/src/main/java/ru/bmstu/lab4/StoreType.java
@@ -1,4 +1,26 @@
 package ru.bmstu.lab4;
 
 public class StoreType {
+
+    private String packageId;
+    private String testName;
+    private String result;
+
+    public StoreMessage (String packageId, String testName, String result){
+        this.packageId = packageId;
+        this.result = result;
+        this.testName = testName;
+    }
+
+    public String getPackageId(){
+        return packageId;
+    }
+
+    public String getTestName(){
+        return testName;
+    }
+
+    public String getResult(){
+        return result;
+    }
 }
