381dda395845f055991c1f676524c12ef5683cea
fedotova <fedotovayo@gmail.com>
2020-12-15 21:36:17 +0300

    Scripted auto-commit on change (2020-12-15 21:36:17) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ContentTests.java
+++ lab4/src/main/java/ru/bmstu/lab4/ContentTests.java
@@ -1,5 +1,9 @@
 package ru.bmstu.lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ContentTests {
-    
+    @JsonProperty("testName") private final String testName;
+    @JsonProperty ("expectedResult") private final String expectedResult;
+    @JsonProperty ("params") private final ArrayList 
 }
