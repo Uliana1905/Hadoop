e17a9e7b05275c742315c4231beacd45a46b9e4f
fedotova <fedotovayo@gmail.com>
2020-12-15 21:12:58 +0300

    Scripted auto-commit on change (2020-12-15 21:12:58) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ExecutorActor.java
+++ lab4/src/main/java/ru/bmstu/lab4/ExecutorActor.java
@@ -1,9 +1,12 @@
 package ru.bmstu.lab4;
 
+
 import akka.actor.AbstractActor;
 
-public class ExecutorActor {
-    public Receieve createRecieve(){}
-    
+public class ExecutorActor extends AbstractActor {
+    public Receive createReceive(){
+        return null;
+    }
+
 
 }
