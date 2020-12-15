701779fb7bb7ea1cf089fe6cf7f336bac9926afa
fedotova <fedotovayo@gmail.com>
2020-12-15 20:11:43 +0300

    Scripted auto-commit on change (2020-12-15 20:11:43) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
+++ lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
@@ -11,7 +11,7 @@ import static akka.actor.SupervisorStrategy.restart;
 
 public class ActorRouter extends AbstractActor{
 
-    private final ActorRef Executors = getContext().actorOf(new RoundRobinPool(5).props(Props.create(ExecutorActor.class)));
+    private final ActorRef executors = getContext().actorOf(new RoundRobinPool(5).props(Props.create(ExecutorActor.class)));
     private final ActorRef storActor = getContext().actorOf(Props.create(StoreActor.class));
    // private static ActorAttributes.SupervisionStrategy strategy =
       ///      new OneForOneStrategy(10, Duration.ofSeconds(10), DeciderBuilder.matchAny(o -> restart()).build());
@@ -22,8 +22,8 @@ public class ActorRouter extends AbstractActor{
 
     public Receive createReceive(){
         return ReceiveBuilder.create()
-                .match(TypeResult.class, msg -> storActor.tell ( msg, sender()).build())
-                .match(StoreMessage.class, ExecutorActor.tell(msg, ActoRef.nosender()));
+                .match(StoreMessage.class, msg -> executors.tell(msg, ActorRef.noSender()))
+                .match(TypeResult.class, msg -> storActor.tell ( msg, sender())).build();
     }
 }
 
