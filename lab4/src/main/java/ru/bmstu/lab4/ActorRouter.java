17fbc3cfbd7f0d053e597e9faff89cc9107c4b68
fedotova <fedotovayo@gmail.com>
2020-12-15 18:08:15 +0300

    Scripted auto-commit on change (2020-12-15 18:08:15) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
+++ lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
@@ -1,11 +1,17 @@
 package ru.bmstu.lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.OneForOneStrategy;
 import akka.actor.Props;
 import akka.routing.RoundRobinPool;
+import akka.stream.ActorAttributes;
 
 public class ActorRouter extends AbstractActor{
 
     private final ActorRef Executors = getContext().actorOf(new RoundRobinPool(5).props(Props.create(ExecuteActor,class)));
     private final ActorRef storActor = getContext().actorOf(Props.create(StoreActor.class));
+    private static ActorAttributes.SupervisionStrategy strategy =
+            new OneForOneStrategy(10,)
+
 }
+
