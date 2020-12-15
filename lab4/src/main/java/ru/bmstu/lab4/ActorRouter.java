17d87c119ebbf6c8c9eb7938f0606021b6ee7567
fedotova <fedotovayo@gmail.com>
2020-12-15 18:27:27 +0300

    Scripted auto-commit on change (2020-12-15 18:27:27) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
+++ lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
@@ -22,7 +22,7 @@ public class ActorRouter extends AbstractActor{
 
     public Receive createReceive(){
         return ReceiveBuilder.create()
-                .match(GetMessage.class, msg -> )
+                .match(GetMessage.class, msg -> StoreActor.class)
     }
 }
 
