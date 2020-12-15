da39658d98f2ec44e606309a80734fff2bf178ad
fedotova <fedotovayo@gmail.com>
2020-12-15 13:14:51 +0300

    Scripted auto-commit on change (2020-12-15 13:14:51) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ReceiveJSON.java
+++ lab4/src/main/java/ru/bmstu/lab4/ReceiveJSON.java
@@ -1,15 +1,31 @@
 package ru.bmstu.lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 
 public class ReceiveJSON {
     @JsonProperty("packageId")
     private final String packageId;
     @JsonProperty ("jsScript")
     private final String jsScript;
-    @JsonProperty ("finctionName")
+    @JsonProperty ("functionName")
     private final String functionName;
     @JsonProperty ("tests")
-    private final ArrayLists
+    private final ArrayLists<> tests;
+
+    @JsonCreator
+    public ReceiveJSON(@JsonProperty("packageId") String packageId,
+                       @JsonProperty("jsScript") String jsScript,
+                       @JsonProperty("functionName") String functionName,
+                       @JsonProperty ("tests")ArrayList<> tests){
+        this.packageId = packageId;
+        this.functionName = functionName;
+        this.jsScript = jsScript;
+        this.tests = tests;
+
+    }
+    public 
 
 }
