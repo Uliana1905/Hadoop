0cb1e76b2f55a08fd54fc5720288703e6b679dee
fedotova <fedotovayo@gmail.com>
2020-12-15 18:11:42 +0300

    Scripted auto-commit on change (2020-12-15 18:11:42) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
+++ lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
@@ -3,15 +3,18 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.OneForOneStrategy;
 import akka.actor.Props;
+import akka.japi.pf.DeciderBuilder;
 import akka.routing.RoundRobinPool;
 import akka.stream.ActorAttributes;
+
+import java.time.Duration;
 
 public class ActorRouter extends AbstractActor{
 
     private final ActorRef Executors = getContext().actorOf(new RoundRobinPool(5).props(Props.create(ExecuteActor,class)));
     private final ActorRef storActor = getContext().actorOf(Props.create(StoreActor.class));
     private static ActorAttributes.SupervisionStrategy strategy =
-            new OneForOneStrategy(10,Duraction.ofSeconds(10))
+            new OneForOneStrategy(10, Duration.ofSeconds(10), DeciderBuilder.matchAny(o -> restart().build));
 
 }
 
