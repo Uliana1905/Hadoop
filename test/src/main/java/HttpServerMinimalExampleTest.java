56ca01da2c393b9962927e94c363c5b43e038b12
fedotova <fedotovayo@gmail.com>
2020-12-15 16:31:16 +0300

    Scripted auto-commit on change (2020-12-15 16:31:16) by gitwatch.sh

--- test/src/main/java/HttpServerMinimalExampleTest.java
+++ test/src/main/java/HttpServerMinimalExampleTest.java
@@ -1,10 +1,11 @@
-
+package ru.bmstu.javascript.tester;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
@@ -12,42 +13,52 @@ import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-public class HttpServerMinimalExampleTest extends AllDirectives {
+public class JavaScriptTester extends AllDirectives {
+    public static void main(String[] args) throws ScriptException, NoSuchMethodException, IOException {
 
-    public static void main(String[] args) throws Exception {
-        // boot up server using the route as defined below
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+//        engine.eval("var divideFn = function(a,b) { return a/b}");
+//        Invocable invocable = (Invocable) engine;
+//        Object[] params = {2, 1};
+//        System.out.println(invocable.invokeFunction("divideFn", params).toString());
+
         ActorSystem system = ActorSystem.create("routes");
-
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-
-        //In order to access all directives we need an instance where the routes are define.
-        HttpServerMinimalExampleTest app = new HttpServerMinimalExampleTest();
-
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost("localhost", 8080), materializer);
+        JavaScriptTester instance = new JavaScriptTester();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer);
 
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
-
+        System.in.read();
         binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
     }
 
     private Route createRoute() {
         return route(
-                path("hello", () ->
-                        get(() ->
-                                complete("<h1>Say hello to akka-http</h1>"))),
-                path("test", () ->
-                        get(() ->
-                                complete("<h1>ты на дружочке, пирожочке </h1>"))),
-                path( "poster", () ->
-                        post(() ->
-                                complete("tester "))));
+                get(
+                        () -> parameter("packageId",
+                                (id) -> complete(""+ id + "\n"))
+                ),
+                post(
+                        () -> entity(Jackson.unmarshaller(RequestBody.class),
+                                (requestBody) -> complete(requestBody.getFunctionName()))
+                )
+        );
     }
+
 }
