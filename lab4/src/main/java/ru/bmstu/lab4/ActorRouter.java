128b1d5a59d1755123e21f7c508c213c104f329f
fedotova <fedotovayo@gmail.com>
2020-12-15 18:06:58 +0300

    Scripted auto-commit on change (2020-12-15 18:06:58) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
+++ lab4/src/main/java/ru/bmstu/lab4/ActorRouter.java
@@ -2,9 +2,10 @@ package ru.bmstu.lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.routing.RoundRobinPool;
 
 public class ActorRouter extends AbstractActor{
 
     private final ActorRef Executors = getContext().actorOf(new RoundRobinPool(5).props(Props.create(ExecuteActor,class)));
-    private final ActorRef storActor = get ().actorOf(Props.create(StoreActor.class));
+    private final ActorRef storActor = getContext().actorOf(Props.create(StoreActor.class));
 }
