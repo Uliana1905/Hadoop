7b5506059e6a2cc422ef5e2617531c9969544e3f
fedotova <fedotovayo@gmail.com>
2020-12-15 13:09:54 +0300

    Scripted auto-commit on change (2020-12-15 13:09:54) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ReceiveJSON.java
+++ lab4/src/main/java/ru/bmstu/lab4/ReceiveJSON.java
@@ -1,5 +1,15 @@
 package ru.bmstu.lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ReceiveJSON {
-    
+    @JsonProperty("packageId")
+    private final String packageId;
+    @JsonProperty ("jsScript")
+    private final String jsScript;
+    @JsonProperty ("finctionName")
+    private final String functionName;
+    @JsonProperty ("tests")
+    private final ArrayLists
+
 }
