f0e5c92729e2219f27d828d23331a326756747b6
fedotova <fedotovayo@gmail.com>
2020-12-16 13:49:08 +0300

    Scripted auto-commit on change (2020-12-16 13:49:08) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ExecutorActor.java
+++ lab4/src/main/java/ru/bmstu/lab4/ExecutorActor.java
@@ -3,12 +3,19 @@ package ru.bmstu.lab4;
 
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class ExecutorActor extends AbstractActor {
 
 
 
 
-    
+    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    engine.eval(jscript);
+    Invocable invocable = (Invocable) engine;
+    return invocable.invokeFunction(functionName, params).toString();
 
     public Receive createReceive()
     {
