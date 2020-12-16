1e2a02b907c22dcafb4adb4c18c003a24bfc7ba5
fedotova <fedotovayo@gmail.com>
2020-12-16 16:33:48 +0300

    Scripted auto-commit on change (2020-12-16 16:33:48) by gitwatch.sh

--- lab4/src/main/java/ru/bmstu/lab4/ExecutorActor.java
+++ lab4/src/main/java/ru/bmstu/lab4/ExecutorActor.java
@@ -12,12 +12,16 @@ import javax.script.ScriptException;
 
 public class ExecutorActor extends AbstractActor {
 
-    
+    private final static String STR_FUNCTION_NAME = "functionName: ";
 
-
+    private final static String STR_RESULT = "result = ";
+    private final static String STR_TRUE = ".True";
+    private final static String STR_FALSE = ".False";
+    private final static String STR_CORRECT_RESULT = " Correct result: ";
+    private final static String SHORTNAME = "nashorn";
 
     public String SolveTask (oneTest one_test) throws ScriptException, NoSuchMethodException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(SHORTNAME);
         engine.eval(one_test.getJsScript());
         Invocable invocable = (Invocable) engine;
         return invocable.invokeFunction(one_test.getFunctionName(), one_test.getParams().toArray()).toString();
@@ -26,9 +30,9 @@ public class ExecutorActor extends AbstractActor {
     public String CompareValue(oneTest one_test) throws  ScriptException, NoSuchMethodException{
         String myres = SolveTask(one_test);
         if (one_test.getExpectedResult().equals(myres)){
-            return "functionName: " + one_test.getFunctionName() + "result = " + one_test.getExpectedResult() + ".True\n";
+            return STR_FUNCTION_NAME + one_test.getFunctionName() + STR_RESULT + one_test.getExpectedResult() + STR_TRUE;
         }else{
-            return "functionName: " + one_test.getFunctionName() + "result = " + one_test.getExpectedResult() + ".False" + " Correct result: " + myres;
+            return STR_FUNCTION_NAME + one_test.getFunctionName() + STR_RESULT + one_test.getExpectedResult() + STR_FALSE + STR_CORRECT_RESULT + myres;
         }
     }
 
