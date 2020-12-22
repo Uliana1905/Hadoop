926471b1ad1ba5569fd872282162013d66101451
fedotova <fedotovayo@gmail.com>
2020-12-22 20:55:20 +0300

    Scripted auto-commit on change (2020-12-22 20:55:20) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/DefSystem.java
+++ lab5/src/main/java/ru/lab5/DefSystem.java
@@ -43,6 +43,7 @@ public class DefSystem {
                 .map(
                          (msg) -> {
                              Query query = msg.getUri().query();
+                             String URL = query.get()
                          }
                 )
     }
