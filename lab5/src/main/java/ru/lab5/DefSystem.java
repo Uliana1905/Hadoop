4cf01b6dcb7a8c7e920d8faeb4a1b0f9f5a5c3ab
fedotova <fedotovayo@gmail.com>
2020-12-22 22:41:10 +0300

    Scripted auto-commit on change (2020-12-22 22:41:10) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/DefSystem.java
+++ lab5/src/main/java/ru/lab5/DefSystem.java
@@ -60,7 +60,7 @@ public class DefSystem {
                                              if ((Integer)answer!=0) {
                                                  return CompletableFuture.completedFuture(new Pair<>(pair.first(),(Integer)answer));
                                              }
-                                             Flow
+                                             Flow<Pair<String, Integer>,Integer, NotUsed> flow = Flow.<Pair<String,Integer>> create()
                                          })
                         })
     }
