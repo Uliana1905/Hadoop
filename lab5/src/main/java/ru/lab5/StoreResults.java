d8a30b670eaf5797fcf5aab5fd9ffe5992980ea9
fedotova <fedotovayo@gmail.com>
2020-12-22 22:30:51 +0300

    Scripted auto-commit on change (2020-12-22 22:30:51) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/StoreResults.java
+++ lab5/src/main/java/ru/lab5/StoreResults.java
@@ -1,4 +1,19 @@
 package ru.lab5;
 
 public class StoreResults {
+    private String Url;
+    private Integer result;
+
+    public StoreResults(String url, Integer result){
+        this.Url = url;
+        this.result = result;
+    }
+
+    public String getUrl(){
+        return Url;
+    }
+
+    public Integer getResult(){
+        return result;
+    }
 }
--- lab5/src/main/java/ru/lab5/storeActor.java
+++ lab5/src/main/java/ru/lab5/storeActor.java
@@ -23,7 +23,7 @@ public class storeActor extends AbstractActor{
                         getSender().tell(0, ActorRef.noSender());
                     }
                 })
-                .match()
+                .match(StoreResults)
     }
 
 }
