01f42e7c1ddb6c79c42d9c3b367d080167307140
fedotova <fedotovayo@gmail.com>
2020-12-22 22:47:00 +0300

    Scripted auto-commit on change (2020-12-22 22:47:00) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/DefSystem.java
+++ lab5/src/main/java/ru/lab5/DefSystem.java
@@ -64,7 +64,7 @@ public class DefSystem {
                                              }
                                              Flow<Pair<String, Integer>,Integer, NotUsed> flow = Flow.<Pair<String,Integer>> create()
                                                      .mapConcat(p ->{
-                                                         List<String> list = Collections.nCopies()
+                                                         List<String> list = Collections.nCopies(p.second(), p.first())
                                                      })
                                          })
                         })
