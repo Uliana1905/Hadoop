df4186e3cf922efab7749e34919a5464fdfd774f
fedotova <fedotovayo@gmail.com>
2020-12-22 22:01:58 +0300

    Scripted auto-commit on change (2020-12-22 22:01:58) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/storeActor.java
+++ lab5/src/main/java/ru/lab5/storeActor.java
@@ -1,9 +1,18 @@
 package ru.lab5;
 
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
 import java.util.HashMap;
 
 public class storeActor {
 
-    private HashMap <String,Integer> store =  new HashMap<>(); 
+    private HashMap <String,Integer> store =  new HashMap<>();
+
+    @Override
+    public AbstractActor.Receive createReceive(){
+        return ReceiveBuilder.create()
+                .match()
+    }
 
 }
