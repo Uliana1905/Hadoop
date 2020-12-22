1c45c6fa2b42304d4950a7e4e80ba42a4c41b318
fedotova <fedotovayo@gmail.com>
2020-12-22 21:58:13 +0300

    Scripted auto-commit on change (2020-12-22 21:58:13) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/storeActor.java
+++ lab5/src/main/java/ru/lab5/storeActor.java
@@ -1,7 +1,9 @@
 package ru.lab5;
 
+import java.util.HashMap;
+
 public class storeActor {
 
-    private HashMap <String,Integer>
+    private HashMap <String,Integer> store =  new HashMap<>(); 
 
 }
