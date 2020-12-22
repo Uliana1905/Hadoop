4a9382b06a1569b20b550ddee15b3d5fac13d5e5
fedotova <fedotovayo@gmail.com>
2020-12-22 19:51:24 +0300

    Scripted auto-commit on change (2020-12-22 19:51:24) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/DefSystem.java
+++ lab5/src/main/java/ru/lab5/DefSystem.java
@@ -1,5 +1,31 @@
 package ru.lab5;
 
+import com.sun.deploy.net.HttpRequest;
+
+import java.io.IOException;
+
 public class DefSystem {
-    
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
+        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown
+        when done
+    }
+
 }
