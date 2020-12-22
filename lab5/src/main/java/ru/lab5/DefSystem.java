8f2cd1fe8c8972ca27d260bad9a0cf18725ed153
fedotova <fedotovayo@gmail.com>
2020-12-22 21:16:29 +0300

    Scripted auto-commit on change (2020-12-22 21:16:29) by gitwatch.sh

--- lab5/src/main/java/ru/lab5/DefSystem.java
+++ lab5/src/main/java/ru/lab5/DefSystem.java
@@ -49,7 +49,7 @@ public class DefSystem {
                              return new Pair<>(URL, count);
                          }).mapAsync(
                                  1, (Pair<> pair) -> {
-                                 SentActorMsg
+                                 SentActorMsg newMes = 
                         })
     }
 
